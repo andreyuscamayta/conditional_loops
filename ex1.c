435c3d70de637dadbaafb3bd5f92b5d8cf4da0e5
Omar Mozo <78625581+andreyuscamayta@users.noreply.github.com>
2021-03-23 21:34:09 -0300

    Update ex1.c
    updated

--- ex1.c
+++ ex1.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main(){
-  printf("\t#SCRATCH#%s\n","XXXXXXX");
-  for(int i = 1;i<13;i++)
-    printf("\t%d x %d = %d\n",i,3,i*3);
+  printf("\t#SCRATCH#%s\n","XXXXXXX");  
   return 0;
 }
